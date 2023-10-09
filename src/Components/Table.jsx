c491afe87a32adabe9f238197982aac8d9a0f68c
Alok_Pal <113025169+Alok-Pal@users.noreply.github.com>
2023-10-09 20:30:30 +0530

    Update Table.jsx

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -14,6 +14,7 @@ import "./table.css";
 import { useNavigate } from "react-router-dom";
 import { LogoutOutlined } from "@mui/icons-material";
 import toast from "react-hot-toast";
+// import image from '../../public/assets/gifs/back.jpg'
 
 const UserTable = () => {
   const [tableData, setTableData] = useState(null);
@@ -28,9 +29,7 @@ const UserTable = () => {
   const dispatch = useDispatch();
   const state = useSelector((state) => state?.userTask?.data);
   const loading = useSelector((state) => state?.userTask?.isLoading);
-  console.log("🚀 ~ file: Table.jsx:31 ~ UserTable ~ loading:", loading)
-  
-  console.log(state,'state')
+
   const navigate = useNavigate();
   const logoutHandler = () => {
     localStorage.clear();
@@ -68,21 +67,18 @@ const UserTable = () => {
 
   // deleteHandler
   const deleteDataHandler = (data) => {
-    console.log("🚀 ~ file: Table.jsx:49 ~ deleteHandler ~ data:", data);
     setDeleteConnectionId(data?.id);
     setisDeleteModalOpen(true);
   };
   const deleteHandler = () => {
     if (deleteConnectionId) {
       dispatch(deleteUserTaskAction(deleteConnectionId)).then((res) => {
-        console.log("🚀 ~ file: Table.jsx:60 ~ dispatch ~ res:", res);
         if (res?.type === "deleteUserTaskAction/fulfilled") {
           dispatch(userTaskAction());
-          toast.success("Task Deleted Successfully",'success');
+          toast.success("Task Deleted Successfully", "success");
           setisDeleteModalOpen(false);
-        }
-        else{
-          toast.error("You are not Authorized to delete",'error');
+        } else {
+          toast.error("You are not Authorized to delete", "error");
           setisDeleteModalOpen(false);
         }
       });
@@ -96,102 +92,107 @@ const UserTable = () => {
 
   return (
     <>
-      <div className="flex justify-content-end mt-2 pe-4">
-        <div
-          className=" btn "
-          style={{ backgroundColor: "lightpink" }}
-          onClick={logoutHandler}
-        >
-          {" "}
-          <LogoutOutlined />
+      <div style={{width: "99.9vw", height:'98vh', margin:'0px',padding:0, background: 'rgb(34,193,195)',background: 'linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(225,203,154,1) 100%)'}} >
+        <div className="flex justify-content-end mt-2 pe-4 ">
+          <div
+            className=" btn mt-5 "
+            style={{ backgroundColor: "lightpink" }}
+            onClick={logoutHandler}
+          >
+            {" "}
+            <LogoutOutlined />
+          </div>
         </div>
-      </div>
-      <div className="text-center">
-        <div className="">
-          {" "}
-          <h3 className="mt-2" style={{color:'white'}}>Add Task's</h3>
-        </div>
-        <button
-          className=" px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
-          onClick={() => {
-            showModal();
-            setModelTitle("ADD TASK");
-          }}
-        >
-          New Task
-        </button>
-        <Modal
-          open={isModalOpen}
-          onCancel={handleCancel}
-          centered={true}
-          width={500}
-          footer={false}
-        >
-          <AddTask
-            title={modelTitle}
-            editData={editData}
-            setIsModalOpen={setIsModalOpen}
-            isAddEdit={isAddEdit}
-          />
-        </Modal>
-      </div>
-      <div className="container mt-5" style={{ height: 400, width: "100%" }}>
-        <h4 style={{color:'white'}}>All Tasks:-</h4>
-        <Table
-          dataSource={tableData}
-          loading={loading}
-          pagination={{
-            pageSize: 5,
-          }}
-        >
-          <Column
-            title="Title"
-            dataIndex="title"
-            key="title"
-            className="bg-white"
-          />
-          <Column
-            title="Description"
-            dataIndex="description"
-            key="description"
-            className="bg-white"
-          />
-          <Column
-            title="Action"
-            dataIndex="action"
-            key="action"
-            className="bg-white"
-            width={"15%"}
-            render={(value, data) => {
-              return (
-                <Space size="middle">
-                  <div style={{ marginRight: "2rem" }}>
-                    <EditOutlined style={{fontSize: 20 ,color : 'blue'}}
-                      onClick={(e) => {
-                        setEditData(data);
-                        showModal();
-                        setIsAddEdit(true);
-                        setModelTitle("Edit TASK");
-                      }}
-                    />
-                  </div>
-                  <div style={{ marginRight: "2rem" ,color : 'red'}}>
-                    <DeleteOutlined style={{fontSize: 20}}
-                      onClick={() => {
-                        deleteDataHandler(data);
-                      }}
-                    />
-                  </div>
-                </Space>
-              );
+        <div className="text-center">
+          <div className="">
+            {" "}
+            <h3 className="mt-2">Add Task's</h3>
+          </div>
+          <button
+            className=" px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
+            onClick={() => {
+              showModal();
+              setModelTitle("ADD TASK");
             }}
+          >
+            New Task
+          </button>
+          <Modal
+            open={isModalOpen}
+            onCancel={handleCancel}
+            centered={true}
+            width={500}
+            footer={false}
+          >
+            <AddTask
+              title={modelTitle}
+              editData={editData}
+              setIsModalOpen={setIsModalOpen}
+              isAddEdit={isAddEdit}
+            />
+          </Modal>
+        </div>
+        <div className="container mt-5" style={{ height: 400, width: "100%" }}>
+          <h4>All Tasks:-</h4>
+          <Table
+            dataSource={tableData}
+            loading={loading}
+            pagination={{
+              pageSize: 5,
+            }}
+            
+          >
+            <Column
+              title="Title"
+              dataIndex="title"
+              key="title"
+              className="bg-white"
+            />
+            <Column
+              title="Description"
+              dataIndex="description"
+              key="description"
+              className="bg-white"
+            />
+            <Column
+              title="Action"
+              dataIndex="action"
+              key="action"
+              className="bg-white"
+              width={"15%"}
+              render={(value, data) => {
+                return (
+                  <Space size="middle">
+                    <div style={{ marginRight: "2rem" }}>
+                      <EditOutlined
+                        style={{ fontSize: 20, color: "blue" }}
+                        onClick={(e) => {
+                          setEditData(data);
+                          showModal();
+                          setIsAddEdit(true);
+                          setModelTitle("Edit TASK");
+                        }}
+                      />
+                    </div>
+                    <div style={{ marginRight: "2rem", color: "red" }}>
+                      <DeleteOutlined
+                        style={{ fontSize: 20 }}
+                        onClick={() => {
+                          deleteDataHandler(data);
+                        }}
+                      />
+                    </div>
+                  </Space>
+                );
+              }}
+            />
+          </Table>
+          <ConfirmDelete
+            handleCancel={deleteHandleCancel}
+            isModalOpen={isDeleteModalOpen}
+            deleteHandler={deleteHandler}
           />
-        </Table>
-        <ConfirmDelete
-          handleCancel={deleteHandleCancel}
-          isModalOpen={isDeleteModalOpen}
-          deleteHandler={deleteHandler}
-        />
+        </div>
       </div>
     </>
   );
